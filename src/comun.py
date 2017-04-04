01b1bfed652e5cb65e03b33cf99b97d188c9c354
atareao <lorenzo.carbonell.cerezo@gmail.com>
2017-04-04 21:34:07 +0200

    Added configurarion for MATE

--- src/comun.py
+++ src/comun.py
@@ -22,6 +22,7 @@
 import os
 import locale
 import gettext
+import subprocess
 import sys
 
 __author__ = 'Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>'
@@ -36,6 +37,63 @@ __version__ = '0.1.0'
 def is_package():
     return __file__.find('src') < 0
 
+
+def is_running(process):
+    # From http://www.bloggerpolis.com/2011/05/\
+    # how-to-check-if-a-process-is-running-using-python/
+    # and http://richarddingwall.name/2009/06/18/\
+    # windows-equivalents-of-ps-and-kill-commands/
+    try:  # Linux/Unix
+        s = subprocess.Popen(["ps", "axw"], stdout=subprocess.PIPE)
+    except Exception as e:  # Windows
+        print(e)
+        s = subprocess.Popen(["tasklist", "/v"], stdout=subprocess.PIPE)
+    for x in s.stdout:
+        if re.search(process, x):
+            return True
+    return False
+
+
+def get_desktop_environment():
+    desktop_session = os.environ.get("DESKTOP_SESSION")
+    # easier to match if we doesn't have  to deal with caracter cases
+    if desktop_session is not None:
+        desktop_session = desktop_session.lower()
+        if desktop_session in ["gnome", "unity", "cinnamon", "mate",
+                               "xfce4", "lxde", "fluxbox", "blackbox",
+                               "openbox", "icewm", "jwm",
+                               "afterstep", "trinity", "kde"]:
+            return desktop_session
+        # ## Special cases ##
+        # Canonical sets $DESKTOP_SESSION to Lubuntu rather than
+        # LXDE if using LXDE.
+        # There is no guarantee that they will not do the same with
+        # the other desktop environments.
+        elif "xfce" in desktop_session or\
+                desktop_session.startswith("xubuntu"):
+            return "xfce4"
+        elif desktop_session.startswith("ubuntu"):
+            return "unity"
+        elif desktop_session.startswith("lubuntu"):
+            return "lxde"
+        elif desktop_session.startswith("kubuntu"):
+            return "kde"
+        elif desktop_session.startswith("razor"):  # e.g. razorkwin
+            return "razor-qt"
+        elif desktop_session.startswith("wmaker"):  # eg. wmaker-common
+            return "windowmaker"
+    if os.environ.get('KDE_FULL_SESSION') == 'true':
+        return "kde"
+    elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
+        if "deprecated" not in os.environ.get(
+                'GNOME_DESKTOP_SESSION_ID'):
+            return "gnome2"
+    # From http://ubuntuforums.org/showthread.php?t=652320
+    elif is_running("xfce-mcs-manage"):
+        return "xfce4"
+    elif is_running("ksmserver"):
+        return "kde"
+    return "unknown"
 ######################################
 
 
